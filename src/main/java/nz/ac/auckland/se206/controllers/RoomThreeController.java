e68217d109637c81585f1a723c52f3af62027a1f
Joshua Tan <jtan678@aucklanduni.ac.nz>
2023-09-01 10:40:33 +1200

    feat: Added go back buttons in room 2 and 3 Also removed uncessary code

--- src/main/java/nz/ac/auckland/se206/controllers/RoomThreeController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/RoomThreeController.java
@@ -2,15 +2,21 @@ package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameState;
 import nz.ac.auckland.se206.GameTimer;
+import nz.ac.auckland.se206.SceneManager;
+import nz.ac.auckland.se206.SceneManager.AppUi;
 
 public class RoomThreeController {
   @FXML private Label timerLabel;
+  @FXML private Rectangle goBackRectangle;
+  @FXML private Rectangle door;
 
   public void initialize() {
     System.out.println("RoomThreeController.initialize()");
@@ -25,6 +31,14 @@ public class RoomThreeController {
     // TODO: Change or move this
     GameState.isGameWon = true;
     App.setRoot("winscreen");
+  }
+
+  @FXML
+  public void clickGoBackRectangle(MouseEvent event) throws IOException {
+    System.out.println("go back clicked");
+
+    Parent roomTwoRoot = SceneManager.getUiRoot(AppUi.ROOM_TWO);
+    App.getScene().setRoot(roomTwoRoot);
   }
 
   /**
