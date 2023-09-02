3a284f94133eb0695aacfc428d24d21f1fce6f22
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2023-09-03 11:57:20 +1200

    feat: Combined time and difficulty select pages Added arrow buttons to change difficulty and time select

--- src/main/java/nz/ac/auckland/se206/controllers/StartController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StartController.java
@@ -2,20 +2,28 @@ package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.GameState;
+import nz.ac.auckland.se206.GameTimer;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 import nz.ac.auckland.se206.gpt.openai.ApiProxyException;
 
 public class StartController {
-  @FXML private Label timerLabel;
-  @FXML private Label displayLabel;
-  @FXML private ImageView easyButton;
-  @FXML private ImageView mediumButton;
-  @FXML private ImageView hardButton;
+  @FXML private Label timeLabel;
+  @FXML private Label difficultyLabel;
+  @FXML private Button nextDifficultyButton;
+  @FXML private Button prevDifficultyButton;
+  @FXML private Button nextTimeButton;
+  @FXML private Button prevTimeButton;
+  @FXML private Button startButton;
+
+  private int time = 120;
+  private String[] difficulty = {"Easy", "Medium", "Hard"};
+  private int currDifficulty = 0;
 
   @FXML
   public void initialize() throws ApiProxyException {
@@ -23,22 +31,50 @@ public class StartController {
   }
 
   @FXML
-  public void clickEasyButton(MouseEvent event) {
-    changeToTimeSelect();
+  public void clickNextDifficultyButton(MouseEvent event) {
+    if (this.currDifficulty < 2) {
+      this.currDifficulty += 1;
+      difficultyLabel.setText(difficulty[currDifficulty]);
+    }
   }
 
   @FXML
-  public void clickMediumButton(MouseEvent event) {
-    changeToTimeSelect();
+  public void clickPrevDifficultyButton(MouseEvent event) {
+    if (this.currDifficulty > 0) {
+      this.currDifficulty -= 1;
+      difficultyLabel.setText(difficulty[currDifficulty]);
+    }
   }
 
   @FXML
-  public void clickHardButton(MouseEvent event) {
-    changeToTimeSelect();
+  public void clickNextTimeButton(MouseEvent event) {
+    if (this.time < 360) {
+      this.time += 120;
+      timeLabel.setText(Integer.toString(this.time));
+    }
   }
 
-  private void changeToTimeSelect() {
-    Parent timeSelectRoot = SceneManager.getUiRoot(AppUi.TIME_SELECT);
-    App.getScene().setRoot(timeSelectRoot);
+  @FXML
+  public void clickPrevTimeButton(MouseEvent event) {
+    if (this.time > 120) {
+      this.time -= 120;
+      timeLabel.setText(Integer.toString(this.time));
+    }
+  }
+
+  @FXML
+  public void clickStartButton(MouseEvent event) {
+    changeToRoom();
+  }
+
+  public void changeToRoom() {
+    // Reset the game so the player can lose
+    GameTimer.setInitialTime(this.time);
+    GameState.isGameWon = false;
+    Parent roomRoot = SceneManager.getUiRoot(AppUi.ROOM_ONE);
+    App.getScene().setRoot(roomRoot);
+    GameTimer gameTimer = GameTimer.getInstance();
+
+    gameTimer.startTimer();
   }
 }
