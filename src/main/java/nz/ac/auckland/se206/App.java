997a903599fb392b11f66e643ea8782767573a25
today0-o <sdrift178@gmail.com>
2023-09-09 16:56:28 +1200

    Resolved conflicted materials
    Fixed the App and StartController to resolve the merge conflict

--- src/main/java/nz/ac/auckland/se206/App.java
+++ src/main/java/nz/ac/auckland/se206/App.java
@@ -49,7 +49,7 @@ public class App extends Application {
     SceneManager.addUi(AppUi.ROOM_ONE, loadFxml("roomone"));
     SceneManager.addUi(AppUi.ROOM_TWO, loadFxml("roomtwo"));
     SceneManager.addUi(AppUi.ROOM_THREE, loadFxml("roomthree"));
-    SceneManager.addUi(AppUi.TIME_SELECT, loadFxml("timeselect"));
+    SceneManager.addUi(AppUi.CHAT, loadFxml("chat"));
     SceneManager.addUi(AppUi.START, loadFxml("start"));
     Parent root = SceneManager.getUiRoot(AppUi.START);
     scene = new Scene(root, 600, 470);
--- src/main/java/nz/ac/auckland/se206/controllers/StartController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StartController.java
@@ -2,53 +2,134 @@ package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameState;
+import nz.ac.auckland.se206.GameTimer;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 import nz.ac.auckland.se206.gpt.openai.ApiProxyException;
 
 public class StartController {
-  @FXML private Label timerLabel;
-  @FXML private Label displayLabel;
-  @FXML private ImageView easyButton;
-  @FXML private ImageView mediumButton;
-  @FXML private ImageView hardButton;
+  @FXML private Label timeLabel;
+  @FXML private Label difficultyLabel;
+  @FXML private Label helpLabel;
+  @FXML private Button nextDifficultyButton;
+  @FXML private Button prevDifficultyButton;
+  @FXML private Button nextTimeButton;
+  @FXML private Button prevTimeButton;
+  @FXML private Button startButton;
+
+  private int time = 120;
+  private String[] difficulty = {"Easy", "Medium", "Hard"};
+  private String[] difficultyHelp = {"- Infinite hints! -", "- 5 hints! -", "- 0 hints! -"};
+  private int currDifficulty = 0;
 
   @FXML
   public void initialize() throws ApiProxyException {
     System.out.println("StartController.initialize()");
   }
 
-  @FXML
-  public void clickEasyButton(MouseEvent event) {
-    GameState.medium = false;
-    GameState.hard = false;
-    GameState.easy = true;
-    changeToTimeSelect();
+  // @FXML
+  // public void clickEasyButton(MouseEvent event) {
+  //   GameState.medium = false;
+  //   GameState.hard = false;
+  //   GameState.easy = true;
+  //   changeToTimeSelect();
+  // }
+
+  // @FXML
+  // public void clickMediumButton(MouseEvent event) {
+  //   GameState.easy = false;
+  //   GameState.hard = false;
+  //   GameState.medium = true;
+  //   changeToTimeSelect();
+  // }
+
+  // @FXML
+  // public void clickHardButton(MouseEvent event) {
+  //   GameState.easy = false;
+  //   GameState.medium = false;
+  //   GameState.hard = true;
+  //   changeToTimeSelect();
+  // }
+
+  public void clickNextDifficultyButton() {
+    if (this.currDifficulty < 2) {
+      this.currDifficulty += 1;
+      difficultyLabel.setText(difficulty[currDifficulty]);
+      helpLabel.setText(difficultyHelp[currDifficulty]);
+
+      if (this.currDifficulty == 0) {
+        GameState.easy = true;
+        GameState.medium = false;
+        GameState.hard = false;
+      } else if (this.currDifficulty == 1) {
+        GameState.easy = false;
+        GameState.medium = true;
+        GameState.hard = false;
+      } else if (this.currDifficulty == 2) {
+        GameState.easy = false;
+        GameState.medium = false;
+        GameState.hard = true;
+      }
+    }
   }
 
   @FXML
-  public void clickMediumButton(MouseEvent event) {
-    GameState.easy = false;
-    GameState.hard = false;
-    GameState.medium = true;
-    changeToTimeSelect();
+  public void clickPrevDifficultyButton() {
+    if (this.currDifficulty > 0) {
+      this.currDifficulty -= 1;
+      difficultyLabel.setText(difficulty[currDifficulty]);
+      helpLabel.setText(difficultyHelp[currDifficulty]);
+
+      if (this.currDifficulty == 0) {
+        GameState.easy = true;
+        GameState.medium = false;
+        GameState.hard = false;
+      } else if (this.currDifficulty == 1) {
+        GameState.easy = false;
+        GameState.medium = true;
+        GameState.hard = false;
+      } else if (this.currDifficulty == 2) {
+        GameState.easy = false;
+        GameState.medium = false;
+        GameState.hard = true;
+      }
+    }
   }
 
   @FXML
-  public void clickHardButton(MouseEvent event) {
-    GameState.easy = false;
-    GameState.medium = false;
-    GameState.hard = true;
-    changeToTimeSelect();
+  public void clickNextTimeButton() {
+    if (this.time < 360) {
+      this.time += 120;
+      timeLabel.setText(Integer.toString(this.time) + " sec");
+    }
   }
 
-  private void changeToTimeSelect() {
-    Parent timeSelectRoot = SceneManager.getUiRoot(AppUi.TIME_SELECT);
-    App.getScene().setRoot(timeSelectRoot);
+  @FXML
+  public void clickPrevTimeButton() {
+    if (this.time > 120) {
+      this.time -= 120;
+      timeLabel.setText(Integer.toString(this.time) + " sec");
+    }
+  }
+
+  @FXML
+  public void clickStartButton() {
+    changeToRoom();
+  }
+
+  public void changeToRoom() {
+    // Need to change the difficulty here as well later
+    GameTimer.setInitialTime(this.time);
+    // Reset the game so the player can lose
+    GameState.isGameWon = false;
+    Parent roomRoot = SceneManager.getUiRoot(AppUi.ROOM_ONE);
+    App.getScene().setRoot(roomRoot);
+    GameTimer gameTimer = GameTimer.getInstance();
+
+    gameTimer.startTimer();
   }
 }
