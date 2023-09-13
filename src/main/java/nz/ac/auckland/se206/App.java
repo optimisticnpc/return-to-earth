9c5319beac855a46669d43c1aa5e08b5d5c66727
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2023-09-14 10:49:01 +1200

    fix: room three reinitialised at restart

--- src/main/java/nz/ac/auckland/se206/App.java
+++ src/main/java/nz/ac/auckland/se206/App.java
@@ -158,6 +158,7 @@ public class App extends Application {
   public static void resetRooms() throws IOException {
     try {
       SceneManager.addUi(AppUi.ROOM_TWO, loadFxml("roomtwo"));
+      SceneManager.addUi(AppUi.ROOM_THREE, loadFxml("roomthree"));
       SceneManager.addUi(AppUi.SPACESUIT_PUZZLE, loadFxml("spacesuitpuzzle"));
     } catch (IOException e) {
       e.printStackTrace();
