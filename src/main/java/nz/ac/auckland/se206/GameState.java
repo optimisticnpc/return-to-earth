c9b7271aaec7232a704b6d3a1dacaa08e5589ecb
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2023-09-21 20:52:02 +1200

    fix: room 1 art less clutter Changed red blue green books to just brown Set bouncing ball to show only when clicked

--- src/main/java/nz/ac/auckland/se206/GameState.java
+++ src/main/java/nz/ac/auckland/se206/GameState.java
@@ -59,6 +59,7 @@ public class GameState {
     isSpacesuitRevealed = false;
     isSpacesuitCollected = false;
     isSpacesuitJustCollected = false;
+    isPartFixed = false;
 
     // Get new math puzzles and update room visuals
     App.resetMathQuestions();
