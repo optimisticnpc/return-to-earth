15b33439af3744991e8e6550ea457b89d80aaed4
Joshua Tan <jtan678@aucklanduni.ac.nz>
2023-10-02 18:01:23 +1300

    feat: Fastest times for each difficulty updated on score screen

--- src/main/java/nz/ac/auckland/se206/ScoreScreenInfo.java
+++ src/main/java/nz/ac/auckland/se206/ScoreScreenInfo.java
@@ -1,5 +1,17 @@
 package nz.ac.auckland.se206;
 
 public class ScoreScreenInfo {
-    
+  public static int fastestEasyTimeHundredths = Integer.MAX_VALUE;
+  public static int fastestMediumTimeHundredths = Integer.MAX_VALUE;
+  public static int fastestHardTimeHundredths = Integer.MAX_VALUE;
+
+  public static String formatTime(int hundredths) {
+    if (hundredths == Integer.MAX_VALUE) {
+      return "--:--";
+    }
+    int minutes = hundredths / 6000;
+    int seconds = (hundredths % 6000) / 100;
+    hundredths = hundredths % 100;
+    return String.format("%d:%02d.%02d", minutes, seconds, hundredths);
+  }
 }
--- src/main/java/nz/ac/auckland/se206/controllers/ScoreScreenController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ScoreScreenController.java
@@ -2,11 +2,43 @@ package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
+import nz.ac.auckland.se206.ScoreScreenInfo;
 
 public class ScoreScreenController {
+  @FXML private Label fastestEasyTimeLabel;
+  @FXML private Label fastestMediumTimeLabel;
+  @FXML private Label fastestHardTimeLabel;
+
+  // Add to ScoreScreenController
+  private static ScoreScreenController instance;
+
+  public static void updateFastestTimes() {
+    if (instance != null) {
+      instance.refreshTimes();
+    }
+  }
+
+  public ScoreScreenController() {
+    instance = this;
+  }
+
+  @FXML
+  public void initialize() {
+    refreshTimes();
+  }
+
+  public void refreshTimes() {
+    fastestEasyTimeLabel.setText(
+        ScoreScreenInfo.formatTime(ScoreScreenInfo.fastestEasyTimeHundredths));
+    fastestMediumTimeLabel.setText(
+        ScoreScreenInfo.formatTime(ScoreScreenInfo.fastestMediumTimeHundredths));
+    fastestHardTimeLabel.setText(
+        ScoreScreenInfo.formatTime(ScoreScreenInfo.fastestHardTimeHundredths));
+  }
 
   @FXML
   private void onGoBack() {
