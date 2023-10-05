07dd3c14e0e0b7e35405749b5f96a2b42f3ccea4
Joshua Tan <jtan678@aucklanduni.ac.nz>
2023-10-06 12:54:44 +1300

    feat: Added hints to maths questions to possibly use for prompting later

--- src/main/java/nz/ac/auckland/se206/MathQuestion.java
+++ src/main/java/nz/ac/auckland/se206/MathQuestion.java
@@ -3,10 +3,19 @@ package nz.ac.auckland.se206;
 public class MathQuestion {
   private String question;
   private String answer;
+  private String hint1;
+  private String hint2;
 
   public MathQuestion(String question, String answer) {
     this.question = question;
     this.answer = answer;
+  }
+
+  public MathQuestion(String question, String answer, String hint1, String hint2) {
+    this.question = question;
+    this.answer = answer;
+    this.hint1 = hint1;
+    this.hint2 = hint2;
   }
 
   public String getQuestion() {
@@ -15,5 +24,13 @@ public class MathQuestion {
 
   public String getAnswer() {
     return answer;
+  }
+
+  public String getHint1() {
+    return hint1;
+  }
+
+  public String getHint2() {
+    return hint2;
   }
 }
